80b13a3c2daf42fa7dca170c3192557a29b71322
Hins Jain <hinsj@maveric-systems.com>
2023-01-10 16:43:25 +0530

    Added Get User By ID functionality

--- src/main/java/com/maveric/userservice/controller/UserController.java
+++ src/main/java/com/maveric/userservice/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.maveric.userservice.controller;
 
+import com.maveric.userservice.model.User;
 import com.maveric.userservice.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,10 @@ public class UserController {
     public UserController(UserService userService) {
         super();
         this.userService = userService;
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") long id){
+        return new ResponseEntity<User>(userService.getUserById(id), HttpStatus.OK);
     }
 }
