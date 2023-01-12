a778a4a8610bc47ec981e06809bfec857315ab25
Hins Jain <hinsj@maveric-systems.com>
2023-01-12 10:40:15 +0530

    Added exception for user not found

--- src/main/java/com/maveric/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/maveric/userservice/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.maveric.userservice.service.impl;
 
+import com.maveric.userservice.exception.UserNotFoundException;
 import com.maveric.userservice.repository.UserRepository;
 import com.maveric.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser(long id) {
+        userRepository.findById(id).orElseThrow(
+                ()->new UserNotFoundException("User not found with id " + id));
         userRepository.deleteById(id);
     }
 }
