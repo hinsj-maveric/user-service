ff4de1a4ca34b1349b3b29105b11f912e1e2bb55
Hins Jain <hinsj@maveric-systems.com>
2023-01-23 11:04:09 +0530

    Solved SonarLint issues

--- src/main/java/com/maveric/userservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/maveric/userservice/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
