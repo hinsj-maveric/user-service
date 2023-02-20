1e6e9796810ee3898e95ac718ffce9a654a06361
Hins Jain <hinsj@maveric-systems.com>
2023-02-20 20:01:40 +0530

    Changed configuration for docker container

--- src/main/java/com/maveric/userservice/feignclient/FeignUserService.java
+++ src/main/java/com/maveric/userservice/feignclient/FeignUserService.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "user-service", url = "http://localhost:3010/api/v1")
+@FeignClient(value = "account-service")
 public interface FeignUserService {
 
-    @DeleteMapping("customers/{customerId}/accounts")
+    @DeleteMapping("api/v1/customers/{customerId}/accounts")
     public ResponseEntity<String> deleteAllAccount(@PathVariable String customerId,
                                                    @RequestHeader(value = "userid") String headerUserId);
 }
